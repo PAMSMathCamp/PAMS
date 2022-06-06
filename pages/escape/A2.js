f2904701dc5d6f2d575a9490946da24562d665e7
Roger Fan <rogerjoeyfan@gmail.com>
2022-06-05 23:00:07 -0700

    Add all 5 forms

--- pages/escape/A2.js
+++ pages/escape/A2.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import EscapeForm from "../../components/form";
 
 export default function A2() {
---
+++ pages/escape/B2.js
@@ -0,0 +1,13 @@
+import EscapeForm from "../../components/form";
+
+export default function B2() {
+
+    return (
+        <EscapeForm
+            title="B2"
+            instruction="Submit your answer from B2."
+            answer={"84"}
+            next="Your next task is difficult. To find B, the second number of the combination lock, you must first find the 3 problems that are hidden under the desks in this room. Solve all 3, and then add all 3 answers together. The sum is B. Once you are done, submit B to the next computer."
+        />
+    )
+}
