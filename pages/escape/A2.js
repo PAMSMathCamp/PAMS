edc8875689304137528691638e8ca3a4590815d5
Roger Fan <rogerjoeyfan@gmail.com>
2022-06-05 22:51:48 -0700

    A2

---
+++ pages/escape/A2.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import EscapeForm from "../../components/form";
+
+export default function A2() {
+
+    return (
+        <EscapeForm 
+            title="A1 and A2" 
+            instruction="Submit the sum of your answer from A1 with your answer from A2."
+            answer={26} 
+            next="Proceed to A3; You will use exactly 11 matchsticks in that problem."
+        />
+    )
+}
