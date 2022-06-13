9f7fa359cd65765f08cf88f0b1853a3ceffa24a2
Roger Fan <rogerjoeyfan@gmail.com>
2022-06-12 21:34:41 -0700

    change B3

--- pages/escape/B2.js
+++ pages/escape/B2.js
@@ -7,7 +7,7 @@ export default function B2() {
             title="B2"
             instruction="Submit your answer from B2."
             answer={"84"}
-            next="Your next task is difficult. To find B, the second number of the combination lock, you must first find the 3 problems that are hidden under the desks in this room. Solve all 3, and then add all 3 answers together. The sum is B. Once you are done, submit B to the next computer."
+            next="Your next task is to take 1 bag (per team) of slips of paper. In the bag, you will find 3 separate math problems. To find B, the second number of the combination lock, solve all 3 problems, and then add all 3 answers together. The sum is B. Once you are done, submit B to the next computer."
         />
     )
 }
